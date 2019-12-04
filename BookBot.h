a7e84fbc6f2db06ee9267b839b653926f2cca0ab
pemami2 <51972919+pemami2@users.noreply.github.com>
2019-12-03 17:27:37 -0800

    Update BookBot.h

--- BookBot.h
+++ BookBot.h
@@ -60,12 +60,12 @@ void BookBot::readIn(const std::string& filename) {
 
 		char punc = word[word.size() - 1]; //saving last letter of string as char
 		if (isEndPunctuation(punc)) { //checking if the punc char is punctuation
-			string punctuation(1, word[word.size() - 1]); //capturing punctuation from last letter of string 
+			string punctuation(1, word[word.size() - 1]); //capturing punctuation from last letter of string
 			word = word.substr(0, word.size() - 1); // resaving word as the string without the last letter
 			markov_chain[word2].push_back(word);// saving word in vector of previous word's key
 			markov_chain[word].push_back(punctuation); //saving the punctuation in vector of word's key
 			markov_chain[punctuation].push_back("$"); //inserting $ in vector corresponding to the punctuation
-			word2 = "^";// starting new sentence 
+			word2 = "^";// starting new sentence
 		}
 		else {
 			markov_chain[word2].push_back(word); // saving the word in the vector belonging to previous word's key
@@ -73,6 +73,7 @@ void BookBot::readIn(const std::string& filename) {
 		}
 	}
 	inFile.close(); // close text file
+	//debug(markov_chain);
 }
 std::vector<std::string> BookBot::getValues(const std::string& key) {
 
@@ -84,17 +85,23 @@ std::string BookBot::generateSentence() {
 	string sentence; // sentence string
 	string new_word; // word following key
 
-	while (key != "?", key != "!", key != ".") { // check to see if at the end of the sentence
-		string new_word = markov_chain[key].at(rand() % (markov_chain[key].size())); // randomly select a word from key's vector and save as new word
-		if (new_word == "." || new_word == "!" || new_word == "?" || key == "^") { //check to see if we are at the begininng or end of sentence
+	while (key != "$") { // check to see if at the end of the sentence
+		string new_word = markov_chain[key].at(rand() % (markov_chain[key].size()));// randomly select a word from key's vector and save as new word
+		if (new_word == "." || new_word == "!" || new_word == "?" || new_word == "^") { //check to see if we are at the begininng or end of sentence
 			sentence = sentence + new_word; // insert the punctuation at the end of the sentence without a space
 			key = new_word; // update the key with the new word
 		}
+		else if (new_word == "$"){
+            key = new_word;
+		}
+
 		else {
 			sentence = sentence + " " + new_word; // insert the new word at the end of the sentence with a space
 			key = new_word; // update the key with the new word
 		}
 	}
 
-	return sentence; // return full sentence 
+    //std::cout<<debug(markov_chain)<<endl;
+	return sentence;// return full sentence
 }
+
